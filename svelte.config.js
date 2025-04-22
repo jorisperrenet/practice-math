d69149abe3d6a7560c491c8b227d285abce1f3bb
nerder <joris.perrenet@gmail.com>
2025-04-22 23:15:19 +0200

    Change to minimal adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
-// import htmlMinifierAdapter from 'sveltekit-html-minifier';
+import htmlMinifierAdapter from 'sveltekit-html-minifier';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,15 +8,12 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
-	// kit: {
-	// 	adapter: htmlMinifierAdapter(adapter())
-	// }
 	kit: {
 	    appDir: 'static',
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+        adapter: htmlMinifierAdapter(adapter()),
 		paths: {
             base: process.env.NODE_ENV === 'production' ? '/practice-math' : '',
         }
